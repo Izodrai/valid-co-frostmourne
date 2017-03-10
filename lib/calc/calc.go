983de65df02c70c2f4d785c46b8577cce24522b6
Valentin Perraudin <vap@capitaldata.fr>
2017-03-10 15:03:36 +0100

    remove useless code

--- lib/calc/calc.go
+++ lib/calc/calc.go
@@ -148,66 +148,6 @@ func Calc_potential(bids []tools.Bid) {
         pos = Position{}
       }
     }
-
-
-    /*
-		if last_ouverture_position == 0.0 {
-			if bid.Macd_signal > bid.Macd_absol_trigger_signal {
-				fmt.Println("Ouverture position à l'achat ->", bid.Bid_at.Format("2006-01-02 15:04:05"), "sur", bid.Last_bid)
-				last_ouverture_position = bid.Last_bid
-				last_ouverture_time = bid.Bid_at
-        achat = true
-			} else if bid.Macd_signal < -bid.Macd_absol_trigger_signal {
-				fmt.Println("Ouverture position à la vente ->", bid.Bid_at.Format("2006-01-02 15:04:05"), "sur", bid.Last_bid)
-				last_ouverture_position = bid.Last_bid
-				last_ouverture_time = bid.Bid_at
-        vente = true
-      }
-		} else {
-      if achat {
-        if bid.Macd_signal <= bid.Macd_absol_trigger_signal {
-  				diff := bid.Last_bid - last_ouverture_position
-  				fmt.Println("Fermeture position à l'achat ->", bid.Bid_at.Format("2006-01-02 15:04:05"), "sur", bid.Last_bid, "avec comme valeur d'ouverture", last_ouverture_position, "à", last_ouverture_time, "gain de :", diff)
-  				fmt.Println("Total des gains jusqu'à présent :", gain)
-  				gain += diff
-  				fmt.Println("Après cette position :", gain)
-  				fmt.Println("#################################################")
-  				last_ouverture_position = 0.0
-  				last_ouverture_time = time.Time{}
-          achat = false
-  				if diff > 0 {
-  					win_pos++
-            win_achat_pos++
-  					tot_win += diff
-  				} else {
-  					lost_pos++
-            lost_achat_pos++
-  					tot_lost += -diff
-  				}
-  			}
-      } else if vente {
-        if bid.Macd_signal >= -bid.Macd_absol_trigger_signal {
-          diff := last_ouverture_position - bid.Last_bid
-  				fmt.Println("Fermeture position à la vente ->", bid.Bid_at.Format("2006-01-02 15:04:05"), "sur", bid.Last_bid, "avec comme valeur d'ouverture", last_ouverture_position, "à", last_ouverture_time, "gain de :", diff)
-  				fmt.Println("Total des gains jusqu'à présent :", gain)
-  				gain += diff
-  				fmt.Println("Après cette position :", gain)
-  				fmt.Println("#################################################")
-  				last_ouverture_position = 0.0
-  				last_ouverture_time = time.Time{}
-          vente = false
-  				if diff > 0 {
-  					win_pos++
-            win_vente_pos++
-  					tot_win += diff
-  				} else {
-  					lost_pos++
-            lost_vente_pos++
-  					tot_lost += -diff
-  				}
-  			}
-      }
-		}*/
 	}
 
   if pos.open && pos.buy {
